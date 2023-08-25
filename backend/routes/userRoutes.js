9b1a8ef2f85be40c9e5dcb3838ad17d22cc11e13
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-08-25 13:40:05 +0400

    cleaned some routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,12 @@
 import express from 'express';
 import { authUser } from '../controlers/userControler.js';
 const router = express.Router();
-import { getAllData,adminRoute,newAboutUsImage,newAboutUsImageALL,newAboutUsImageUpdate } from '../controlers/adminRoutes.js';
+import { getAllData} from '../controlers/adminControler.js';
 import { registerUser } from '../controlers/userControler.js';
-import { isAdmin, protect } from '../middleware/authMiddleware.js';
 
 router.post('/',registerUser)
 router.post('/auth',authUser)
-router.post('/admin', protect,adminRoute)
 router.get('/data',getAllData)
 
-//about-us section
-router.post("/aboutusimage",newAboutUsImage)
-router.get("/aboutusimage",newAboutUsImageALL)
-router.put("/aboutusimage:id",newAboutUsImageUpdate)
-router.put("/aboutusimage:id",newAboutUsImageUpdate)
+
 export default router; 
